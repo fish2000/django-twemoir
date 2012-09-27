81f1ee07e8725ca38fa34643498f11f20bdd4e2c
Alexander Böhn <fish2000@gmail.com>
2012-09-27 01:26:29 -0400

    Removed most of twemoir's view layer (still coupled as it largely was to ost2's assumptions).

--- twemoir/urls.py
+++ twemoir/urls.py
@@ -11,27 +11,9 @@ app_patterns = patterns('',
         'twemoir.views.paragraphs',
         name="grafs"),
 
-    url(r'^bttstrp/?$',
-        'twemoir.views.bootstrap',
-        name="bttstrp"),
-
-    url(r'^txtbxx/?$',
-        'twemoir.views.textbox',
-        name="txtbxx"),
-
     url(r'^/?$',
         'twemoir.views.paragraphs',
         name="root"),
-
-    ####### >> HERE BE OAUTH2 DRAGONS << ########
-
-    url(r'^auth/begin/?$',
-        'twemoir.oauth.begin',
-        name="auth-begin"),
-
-    url(r'^auth/callback/?$',
-        'twemoir.oauth.callback',
-        name="auth-callback"),
 )
 
 # URL namespace
